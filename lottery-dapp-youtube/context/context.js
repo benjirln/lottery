7b13d79e24834606dd9c9eaf36c440e4cefd1ab8
cantfly1 <91459524+cantfly1@users.noreply.github.com>
2023-11-16 14:37:44 +0100

    AB

--- lottery-dapp-youtube/context/context.js
+++ lottery-dapp-youtube/context/context.js
@@ -1,19 +1,32 @@
 import { createContext, useState, useEffect, useContext } from 'react'
-
+import Web3 from 'web3'
+import createlotteryContract from '../utils/lotteryContact'
 export const appContext = createContext()
 
 export const AppProvider = ({ children }) => {
-  const [address, setAddress] = useState(null)
+  const [address, setAddress] = useState('')
+  const [web3, setWeb3] = useState(null)
+  const [Lotterycontract, setLotterycontract] = useState()
   const connectWallet = async () => {
     if( 
       typeof window.ethereum !== 'undefined' &&
       typeof window !== 'undefined'
     ){
       try{
-        const accounts = await window.ethereum.request({
+        await window.ethereum.request({
           method: 'eth_requestAccounts'
         })
+        const web3 = new Web3(window.ethereum)
+        setWeb3(web3)
+        const accounts = await web3.eth.getAccounts()
         setAddress(accounts[0])
+        setLotterycontract(
+          createlotteryContract(web3)
+        )
+        window.ethereum.on('accountsChanged', async () => {
+          const accounts = await web3.eth.getAccounts()
+          setAddress(accounts[0])
+        })
       }catch(error){
         console.log(error)
       }
@@ -25,7 +38,7 @@ export const AppProvider = ({ children }) => {
 
   }
 
-  return <appContext.Provider value={{}}>{children}</appContext.Provider>
+  return <appContext.Provider value={{connectWallet,address}}>{children}</appContext.Provider>
 }
 
 export const useAppContext = () => {
